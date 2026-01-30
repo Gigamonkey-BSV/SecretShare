e860a07c587cb91a1cbc71fe8f6724456f36604d
Daniel Krawisz <danielkrawisz@protonmail.com>
2026-01-30 14:15:45 -0500

    Update with some new stuff in data: error codes and input args

--- secret_share.cpp
+++ secret_share.cpp
@@ -3,10 +3,40 @@
 #include <data/crypto/secret_share.hpp>
 #include <data/encoding/ascii.hpp>
 #include <data/io/random.hpp>
+#include <data/io/main.hpp>
 
-#include <gigamonkey/schema/hd.hpp>
-#include <gigamonkey/schema/random.hpp>
 #include <gigamonkey/p2p/checksum.hpp>
+
+namespace Gigamonkey {
+    std::string secret_share (std::span<const char *const> args);
+}
+
+using error = data::io::error;
+
+namespace data {
+    void signal_handler (int signal) noexcept {
+        exit (0);
+    }
+
+    error main (std::span<const char *const> args) {
+
+        using namespace Gigamonkey;
+
+        try {
+
+            DATA_LOG (normal) << secret_share (args);
+
+        } catch (const exception &ex) {
+
+            std::cout << "Error: " << ex.what () << std::endl;
+
+            return error::code {ex.Code};
+        }
+
+        return error {};
+
+    }
+}
 
 std::ostream inline &operator << (std::ostream &o, const data::crypto::secret_share &x) {
     return o << Gigamonkey::base58::check (static_cast<data::byte> (x.Index), x.Data).encode ();
@@ -18,9 +48,32 @@ namespace data::random {
 
 namespace Gigamonkey {
     
-    std::string secret_share_split (list<string> args) {
+    std::string secret_share_split (std::span<const char *const> args);
+    
+    std::string secret_share_merge (std::span<const char *const> args);
 
-        if (args.size () != 3) throw exception (2) << "3 further arguments required for split";
+    std::string help ();
+
+    std::string version ();
+    
+    std::string secret_share (std::span<const char *const> args) {
+
+        if (args.empty () || args[0] == "help") return help ();
+
+        if (args[0] == "version") return version ();
+
+        if (args[0] == "split") return secret_share_split (args.last (args.size () - 1));
+
+        if (args[0] == "merge") return secret_share_merge (args.last (args.size () - 1));
+
+        return help ();
+
+    }
+
+    std::string secret_share_split (std::span<const char *const> args) {
+
+        if (args.size () != 3)
+            throw exception (error::user_action) << "3 further arguments required for split";
 
         string message = args[0];
 
@@ -30,7 +83,8 @@ namespace Gigamonkey {
             ss >> shares;
         }
 
-        if (shares > 10 || shares == 0) throw exception (4) << "argument 3 (shares) must be less than 10 and greater than zero; " << shares;
+        if (shares > 10 || shares == 0)
+            throw exception (error::user_action) << "argument 3 (shares) must be less than 10 and greater than zero; " << shares;
 
         uint64 threshold;
         {
@@ -38,12 +92,13 @@ namespace Gigamonkey {
             ss >> threshold;
         }
 
-        if (threshold == 0 || threshold > shares) throw exception (6) << "argument 4 (threshold) must be greater than zero and less than shares.";
+        if (threshold == 0 || threshold > shares)
+            throw exception (error::user_action) << "argument 4 (threshold) must be greater than zero and less than shares.";
 
         data::random::init ({.strength = 256});
 
         cross<crypto::secret_share> secret_shares =
-            crypto::secret_share_split (crypto::random::get (), bytes (message), static_cast<uint32> (shares), static_cast<uint32> (threshold));
+        crypto::secret_share_split (crypto::random::get (), bytes (message), static_cast<uint32> (shares), static_cast<uint32> (threshold));
 
         list<string> output;
         for (const crypto::secret_share &x : secret_shares)
@@ -54,10 +109,13 @@ namespace Gigamonkey {
         return ss.str ();
 
     }
-    
-    std::string secret_share_merge (list<string> args) {
 
-        maybe<data::N_bytes_little> threshold_arg = encoding::decimal::read<data::endian::little, byte> (args.first ());
+    std::string secret_share_merge (std::span<const char *const> args) {
+
+        if (args.size () == 0)
+            throw exception (error::user_action) << "no further arguments after 'merge'. First we need the number of shares, followed by shares.";
+
+        maybe<data::N_bytes_little> threshold_arg = encoding::decimal::read<data::endian::little, byte> (args[0]);
 
         uint64 threshold;
         {
@@ -66,17 +124,17 @@ namespace Gigamonkey {
         }
 
         if (threshold == 0 || threshold > args.size () - 1)
-            throw exception (7) <<
-                "threshold must be greater than zero and the number of additional arguments provided must be at least the threshold.";
+            throw exception (error::user_action) <<
+            "threshold must be greater than zero and the number of additional arguments provided must be at least the threshold.";
 
         cross<crypto::secret_share> shares (args.size () - 1);
 
         int i = 0;
-        for (const string &x : args.rest ()) {
+        for (const string &x : args.last (args.size () - 1)) {
             base58::check share = base58::check::recover (x);
-            if (!share.valid ()) throw exception (8) << "could not recover share " << i;
+            if (!share.valid ()) throw exception (error::user_action) << "could not recover share " << i;
             string encoded = share.encode ();
-            if (encoded != x) std::cout << "recovered base 58 check string " << encoded << " from " << x << std::endl;
+            if (encoded != x) DATA_LOG (normal) << "recovered base 58 check string " << encoded << " from " << x << std::endl;
             shares[i] = crypto::secret_share{share.version (), share.payload ()};
             i ++;
         }
@@ -86,7 +144,7 @@ namespace Gigamonkey {
         data::ASCII merged_string {merged};
         // valid ASCII has no negative chars
         if (!merged_string.valid ())
-            throw exception (8) << "invalid string recovered; here it is in hex: " << encoding::hex::write(merged);
+            throw exception (1) << "invalid string recovered; here it is in hex: " << encoding::hex::write (merged);
 
         // add quotes
         std::stringstream ss;
@@ -97,65 +155,13 @@ namespace Gigamonkey {
 
     std::string help () {
         return
-            "\thelp                                                  Show his message.\n"
-            "\tversion                                               Show version.\n"
-            "\tsplit <data:string> <shares:uint> <threshold:uint>    Split data into shares.\n"
-            "\tmerge <threshold:uint> <shares:string...>             Merge shares into data.";
+        "\thelp                                                  Show his message.\n"
+        "\tversion                                               Show version.\n"
+        "\tsplit <data:string> <shares:uint> <threshold:uint>    Split data into shares.\n"
+        "\tmerge <threshold:uint> <shares:string...>             Merge shares into data.";
     }
 
     std::string version () {
         return "1.2";
     }
-    
-    std::string secret_share (list<string> args) {
-
-        if (args.empty () || args.first () == "help") return help ();
-
-        if (args.first () == "version") return version ();
-
-        if (args.first () == "split") return secret_share_split (args.rest ());
-
-        if (args.first () == "merge") return secret_share_merge (args.rest ());
-
-        return help ();
-
-    }
-    
-}
-
-int main (int num_args, char **arg) {
-
-    using namespace Gigamonkey;
-
-    list<string> args;
-    for (int i = 1; i < num_args; i++) {
-        args = args << string {arg[i]};
-    }
-
-    try {
-
-        std::cout << secret_share (args) << std::endl;
-
-    } catch (const exception &ex) {
-
-        std::cout << "Error: " << ex.what () << std::endl;
-
-        return ex.Code;
-    } catch (const std::exception &ex) {
-
-        std::cout << "Error: " << ex.what () << std::endl;
-
-        return 1;
-    } catch (const int &i) {
-        std::cout << "Unknown error code " << i << std::endl;
-
-        return i;
-    } catch (...) {
-        std::cout << "Unknown error detected " << std::endl;
-
-        return 1;
-    }
-
-    return 0;
-
 }
